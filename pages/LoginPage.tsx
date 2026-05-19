dca6f49f0d5a67bc24594c60b124e88ab1856caa
Tanmay Galav <tanmaygalav@gmail.com>
2026-05-19 17:33:34 +0530

    Update LoginPage.tsx

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -8,7 +8,6 @@ const LoginPage: React.FC = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
   const correctPassword = import.meta.env.VITE_PASS;
-  console.log(correctPassword);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
